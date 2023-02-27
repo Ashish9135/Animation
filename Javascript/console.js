d22a21134710b63e77c929e1567d158dde0d87ba
Ashish <ashishkumar20cs50@gmail.com>
2023-02-27 16:16:57 +0530

    remove folder

--- Javascript/console.js
+++
@@ -1,7 +0,0 @@
-// console.log("nyi duniya");
-let links = document.querySelectorAll('a');
-let thirdLink = links[2];
-thirdLink.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('jha');
-});
